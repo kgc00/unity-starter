8b9d1bd1d2c7bf4fa0217260e826f791250b5817
kgc00 <kirbygc00@gmail.com>
2021-07-03 16:11:09 -0700

    initial

---
+++ Assets/Scripts/Input/InputReader.cs
@@ -0,0 +1,29 @@
+﻿using Messages;
+using UniRx;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Input {
+    /**
+     * Receives input from unity's input system - broadcasts inputs as events to listeners
+     * At least one monobehavior must reference the scriptable object for it to load in 
+     */
+    [CreateAssetMenu(fileName = "InputReader", menuName = "Game/InputReader", order = 0)]
+    public class InputReader : ScriptableObject, Controls.IGameplayActions {
+        private Controls controls;
+
+        private void OnEnable() {
+            controls ??= new Controls();
+            controls.Gameplay.SetCallbacks(this);
+            controls.Gameplay.Enable();
+        }
+        
+        public void OnMove(InputAction.CallbackContext context) {
+            var readValue = context.ReadValue<Vector2>();
+            if (context.performed) MessageBroker.Default.Publish(new MoveActionPerformed(readValue));
+            if (context.canceled) MessageBroker.Default.Publish(new MoveActionCanceled());
+        }
+        public void OnLook(InputAction.CallbackContext context) { }
+        public void OnFire(InputAction.CallbackContext context) { }
+    }
+}
---
+++ Assets/Scripts/Messages/MoveActionPerformed.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Messages {
+    public class MoveActionPerformed {
+        public readonly Vector2 Motion;
+        public MoveActionPerformed(Vector2 motion) {
+            Motion = motion;
+        }
+    }
+}
